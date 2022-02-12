079f80b500a7deadc308e6ad2c9385bb5005a188
iliana etaoin <iliana@buttslol.net>
2022-02-12 12:08:22 -0800

    add script for bringing SIX peers up/down one by one

---
+++ sea1/router/files/six-updown.sh
@@ -0,0 +1,14 @@
+#!/bin/ksh
+set -e
+
+if [ "$1" != "up" ] && [ "$1" != "down" ]; then
+    echo "usage: $0 up|down [REASON]"
+    exit 1
+fi
+
+for neighbor in $(grep 'neighbor ' /etc/bgpd.conf | awk '{ print $2 }' | grep -F -e '206.81.8' -e '2001:504:16:' | tail -r); do
+    echo -n "bring $neighbor $1? "
+    read -r prompt
+    [[ "$prompt" = "y" ]] || exit 2
+    ( set -x; bgpctl neighbor "$neighbor" "$1" "$2" )
+done
